0cda4c47ff1ac31277447796d6cdba462d9e637a
Sven Geggus <sven-git@geggus.net>
2020-12-27 18:26:07 +0100

    First Version with SVG and participant numbers

--- main.js
+++ main.js
@@ -10,7 +10,6 @@ window.addEventListener('load', function () {
   for (var r = 0; r < roomnames.length; r++) {
     elems = document.getElementsByClassName(roomnames[r]);
     for (var i = 0; i < elems.length; i++) {
-      console.log(r);
       elems[i].addEventListener("click", on.bind(null, r), false);
       elems[i].style.cursor="pointer";
     }
@@ -23,25 +22,21 @@ function on(num) {
      document.getElementById("mainframe").style.display = "none"
      document.getElementById("videoframe").style.display = "block"
      options.roomName = 'SilvesterParty_2020_'+roomnames[num];
-     console.log(options.roomName);
      api[num] = new JitsiMeetExternalAPI(domain, options);    
      api[num].executeCommand("subject", roomnames[num]);  
 
      api[num].on('readyToClose', () => {
-       console.log(num);
        chatClosed(api,num);
      });
 
      api[num].on('videoConferenceJoined', (arg) => {
        numusers = api[num].getParticipantsInfo().length;
        var rurl=roomurl+'?'+num+'='+numusers;
-       console.log(rurl);
        update_numbers(rurl);
      });
 }
 
 function chatClosed(api,num) {
-  //console.log(api[num]);
   //var iframe = api[num].getIFrame();
   //iframe.parentNode.removeChild(iframe);
   api[num].dispose();
@@ -62,20 +57,28 @@ var options = {
 
 // update number of persons in Room displayed on landingpage
 function update_numbers(url) {
-   console.log('called update_numbers('+url+')');
+   // console.log('called update_numbers('+url+')');   
    var xmlHttp = new XMLHttpRequest();
    xmlHttp.onreadystatechange = function() { 
      if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
        var json_resp = xmlHttp.response;
        for (const room in json_resp) {
-         console.log(room);
-         document.getElementById(room).innerHTML=json_resp[room];
+         var obj = document.getElementsByClassName(roomnames[room]+"_guests");
+         // console.log(obj[0]);
+         if (obj[0] != undefined) {
+           var txt = obj[0].innerHTML;
+           // initial replacement
+           txt = txt.replace("xx", json_resp[room]);
+           // this replacement is for old numbers
+           txt = txt.replace(/[0-9]+/, json_resp[room]);
+           obj[0].innerHTML=txt;
+         }
        }
-       //alert(json.Wohnzimmer);
      }
    }
    xmlHttp.responseType = 'json';
    xmlHttp.open("GET", url, true);
+   xmlHttp.setRequestHeader("Cache-Control", "no-cache, no-store, max-age=0");
    xmlHttp.send(null); 
 }
 
